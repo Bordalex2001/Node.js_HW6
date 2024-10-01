d6db095a2389f28021a00823fe2780375319cbb8
Bordalex2001 <balex0121@gmail.com>
2024-10-01 20:48:35 +0300

    Second commit

--- server2.js
+++ server2.js
@@ -3,34 +3,13 @@ import http from "node:http";
 import path from "node:path";
 import url from "node:url";
 import fs from "node:fs";
+import querystring from "node:querystring";
 const PORT = 3000;
 
-const mimeTypes = {
-    ".css": "text/css",
-    ".js": "text/javascript",
-    ".png": "image/png",
-    ".jpeg": "image/jpeg",
-    ".jpg": "image/jpg"
-}
-
-const getStaticFile = (res, filePath, ext) => {
-    res.setHeader("Content-Type", mimeTypes[ext]);
-    fs.readFile(path.join(".", "public", filePath), (err, data) => {
-        if (err){
-            res.statusCode = 404;
-            res.end();
-        }
-        else{
-            res.end(data);
-        }
-    });
-};
-
 const server = http.createServer((req, res) => {
-    const queryParams = url.parse(req.url, true).query;
-    const url = req.url;
-    console.log(queryParams);
-    switch(url){
+    const myUrl = url.parse(req.url, true);
+    console.log(myUrl);
+    switch(myUrl.pathname){
         case '/':
             res.write(
                 fs.readFileSync(
@@ -38,30 +17,35 @@ const server = http.createServer((req, res) => {
             ));
             res.end();
             break;
-        case '/contacts':
-            res.write(
-                fs.readFileSync(
-                    path.join(import.meta.dirname, "public", "pages", "contacts.html")
-            ));
-            res.end();
-            break;
-        case '/products':
-            console.log(req.url.querystring);
-            res.write(
-                fs.readFileSync(
-                    path.join(import.meta.dirname, "public", "pages", "form.html")
-            ));
-            res.end();
-            break;
-        default:
-            const ext = path.extname(url);
-            if(ext in mimeTypes){
-                getStaticFile(res, url, ext);
-            }
-            else{
-                res.statusCode = 404;
+        case '/form':
+            if(req.method === 'GET'){
+                res.write(
+                    fs.readFileSync(
+                        path.join(import.meta.dirname, "public", "pages", "form.html")
+                ));
                 res.end();
             }
+            else if(req.method === 'POST'){
+                let body = '';
+
+                req.on('data', chunk => {
+                    body += chunk.toString();
+                });
+
+                req.on('end', () => {
+                    const parsedData = querystring.parse(body);
+                    const { login, password } = parsedData;
+         
+                    console.log(`Login: ${login}, Password: ${password}`);
+        
+                    res.writeHead(200, { 'Content-Type': 'text/html; charset=UTF-8' });
+                    res.end('Authorization completed successfully!');
+                });
+            };
+            break;
+        default:
+            res.writeHead(404, { 'Content-Type': 'text/plain;' });
+            res.end(`Page not found`);
             break;
     }
 });
